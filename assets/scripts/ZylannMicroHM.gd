e2a599886d21eb42a81b5bb7d378aa01f044951a
Tom Dobbelaere <tom.dobbelaere@student.howest.be>
2018-04-03 16:37:10 +0200

    Zylann micro changes

--- assets/scripts/ZylannMicroHM.gd
+++ assets/scripts/ZylannMicroHM.gd
@@ -11,8 +11,8 @@ extends Node2D
 # executed 10 times, because there is too big of a difference without
 # taking the average of multiple runs.
 
-const ITERATIONS = 500000
-const AVG_TIMES = 10
+const ITERATIONS = 2000000
+const AVG_TIMES = 10.0
 
 var chronometer = load('res://assets/scripts/Chronometer.gd').new();
 var while_time = 0;
@@ -25,76 +25,78 @@ func _empty_void_function():
 # } Stuff used in tests
 
 func _ready():
-	var test_while_loop = 0;
-	var test_empty_func = 0;
-	var test_increment = 0;
-	var test_increment_x5 = 0;
-	var test_increment_with_member_var = 0;
-	var test_increment_with_local_outside_loop = 0;
-	var test_increment_with_local_inside_loop = 0;
-	var test_increment_vector2 = 0;
-	var test_increment_vector3 = 0;
-	var test_increment_vector3_constant = 0;
-	var test_increment_vector3_individual_xyz = 0;
-	var test_unused_local = 0;
-	var test_divide = 0;
-	var test_increment_with_array_member = 0;
-	var test_if_true = 0;
-	var test_if_true_else = 0;
+	var vtest_while_loop = 0;
+	var vtest_empty_func = 0;
+	var vtest_increment = 0;
+	var vtest_increment_x5 = 0;
+	var vtest_increment_with_member_var = 0;
+	var vtest_increment_with_local_outside_loop = 0;
+	var vtest_increment_with_local_inside_loop = 0;
+	var vtest_increment_vector2 = 0;
+	var vtest_increment_vector3 = 0;
+	var vtest_increment_vector3_constant = 0;
+	var vtest_increment_vector3_individual_xyz = 0;
+	var vtest_unused_local = 0;
+	var vtest_divide = 0;
+	var vtest_increment_with_array_member = 0;
+	var vtest_if_true = 0;
+	var vtest_if_true_else = 0;
 	
 	var avg_count = 0;
 	while avg_count < AVG_TIMES:
-		test_while_loop += test_while_loop();
-		test_empty_func += test_empty_func();
-		test_increment += test_increment();
-		test_increment_x5 += test_increment_x5();
-		test_increment_with_member_var += test_increment_with_member_var();
-		test_increment_with_local_outside_loop += test_increment_with_local_outside_loop();
-		test_increment_with_local_inside_loop += test_increment_with_local_inside_loop();
-		test_increment_vector2 += test_increment_vector2();
-		test_increment_vector3 += test_increment_vector3();
-		test_increment_vector3_constant += test_increment_vector3_constant();
-		test_increment_vector3_individual_xyz += test_increment_vector3_individual_xyz();
-		test_unused_local += test_unused_local();
-		test_divide += test_divide();
-		test_increment_with_array_member += test_increment_with_array_member();
-		test_if_true += test_if_true();
-		test_if_true_else += test_if_true_else();		
+		vtest_while_loop += test_while_loop();
+		vtest_empty_func += test_empty_func();
+		vtest_increment += test_increment();
+		vtest_increment_x5 += test_increment_x5();
+		vtest_increment_with_member_var += test_increment_with_member_var();
+		vtest_increment_with_local_outside_loop += test_increment_with_local_outside_loop();
+		vtest_increment_with_local_inside_loop += test_increment_with_local_inside_loop();
+		vtest_increment_vector2 += test_increment_vector2();
+		vtest_increment_vector3 += test_increment_vector3();
+		vtest_increment_vector3_constant += test_increment_vector3_constant();
+		vtest_increment_vector3_individual_xyz += test_increment_vector3_individual_xyz();
+		vtest_unused_local += test_unused_local();
+		vtest_divide += test_divide();
+		vtest_increment_with_array_member += test_increment_with_array_member();
+		vtest_if_true += test_if_true();
+		vtest_if_true_else += test_if_true_else();		
 		avg_count += 1;
 
-	test_while_loop /= AVG_TIMES;
-	test_empty_func /= AVG_TIMES;
-	test_increment /= AVG_TIMES;
-	test_increment_x5 /= AVG_TIMES;
-	test_increment_with_member_var /= AVG_TIMES;
-	test_increment_with_local_outside_loop /= AVG_TIMES;
-	test_increment_with_local_inside_loop /= AVG_TIMES;
-	test_increment_vector2 /= AVG_TIMES;
-	test_increment_vector3 /= AVG_TIMES;
-	test_increment_vector3_constant /= AVG_TIMES;
-	test_increment_vector3_individual_xyz /= AVG_TIMES;
-	test_unused_local /= AVG_TIMES;
-	test_divide /= AVG_TIMES;
-	test_increment_with_array_member /= AVG_TIMES;
-	test_if_true /= AVG_TIMES;
-	test_if_true_else /= AVG_TIMES;
+	vtest_while_loop /= AVG_TIMES;
+	vtest_empty_func /= AVG_TIMES;
+	vtest_increment /= AVG_TIMES;
+	vtest_increment_x5 /= AVG_TIMES;
+	vtest_increment_with_member_var /= AVG_TIMES;
+	vtest_increment_with_local_outside_loop /= AVG_TIMES;
+	vtest_increment_with_local_inside_loop /= AVG_TIMES;
+	vtest_increment_vector2 /= AVG_TIMES;
+	vtest_increment_vector3 /= AVG_TIMES;
+	vtest_increment_vector3_constant /= AVG_TIMES;
+	vtest_increment_vector3_individual_xyz /= AVG_TIMES;
+	vtest_unused_local /= AVG_TIMES;
+	vtest_divide /= AVG_TIMES;
+	vtest_increment_with_array_member /= AVG_TIMES;
+	vtest_if_true /= AVG_TIMES;
+	vtest_if_true_else /= AVG_TIMES;
 	
-	print("bench test_while_loop, %s\r" % [test_while_loop]);
-	print("bench test_empty_func, %s\r" % [test_empty_func]);
-	print("bench test_increment, %s\r" % [test_increment]);
-	print("bench test_increment_x5, %s\r" % [test_increment_x5]);
-	print("bench test_increment_with_member_var, %s\r" % [test_increment_with_member_var]);
-	print("bench test_increment_with_local_outside_loop, %s\r" % [test_increment_with_local_outside_loop]);
-	print("bench test_increment_with_local_inside_loop, %s\r" % [test_increment_with_local_inside_loop]);
-	print("bench test_increment_vector2, %s\r" % [test_increment_vector2]);
-	print("bench test_increment_vector3, %s\r" % [test_increment_vector3]);
-	print("bench test_increment_vector3_constant, %s\r" % [test_increment_vector3_constant]);
-	print("bench test_increment_vector3_individual_xyz, %s\r" % [test_increment_vector3_individual_xyz]);
-	print("bench test_unused_local, %s\r" % [test_unused_local]);
-	print("bench test_divide, %s\r" % [test_divide]);
-	print("bench test_increment_with_array_member, %s\r" % [test_increment_with_array_member]);
-	print("bench test_if_true, %s\r" % [test_if_true]);
-	print("bench test_if_true_else, %s\r" % [test_if_true_else]);
+	print("bench test_while_loop, %s\r" % [vtest_while_loop]);
+	print("bench test_empty_func, %s\r" % [vtest_empty_func]);
+	print("bench test_increment, %s\r" % [vtest_increment]);
+	print("bench test_increment_x5, %s\r" % [vtest_increment_x5]);
+	print("bench test_increment_with_member_var, %s\r" % [vtest_increment_with_member_var]);
+	print("bench test_increment_with_local_outside_loop, %s\r" % [vtest_increment_with_local_outside_loop]);
+	print("bench test_increment_with_local_inside_loop, %s\r" % [vtest_increment_with_local_inside_loop]);
+	print("bench test_increment_vector2, %s\r" % [vtest_increment_vector2]);
+	print("bench test_increment_vector3, %s\r" % [vtest_increment_vector3]);
+	print("bench test_increment_vector3_constant, %s\r" % [vtest_increment_vector3_constant]);
+	print("bench test_increment_vector3_individual_xyz, %s\r" % [vtest_increment_vector3_individual_xyz]);
+	print("bench test_unused_local, %s\r" % [vtest_unused_local]);
+	print("bench test_divide, %s\r" % [vtest_divide]);
+	print("bench test_increment_with_array_member, %s\r" % [vtest_increment_with_array_member]);
+	print("bench test_if_true, %s\r" % [vtest_if_true]);
+	print("bench test_if_true_else, %s\r" % [vtest_if_true_else]);
+	
+	get_tree().quit();
 	
 	pass
 
